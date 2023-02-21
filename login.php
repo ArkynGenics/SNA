f9940dcb5894a73e6443fa6607fff89c586e292f
ptr173 <pitra173@gmail.com>
2023-02-21 10:33:00 +0700

    update login php

--- login.php
+++ login.php
@@ -52,68 +52,44 @@
 <html>
   <head>
     <title>Login</title>
+    <link rel="stylesheet" href="style1.css">
     <style>
-      body {
-        font-family: Arial, sans-serif;
-        background-color: #f2f2f2;
-      }
-      h1 {
-        text-align: center;
-      }
-      form {
-        max-width: 400px;
-        margin: 0 auto;
-        background-color: #fff;
-        padding: 20px;
-        border-radius: 5px;
-        box-shadow: 0 2px 5px rgba(0,0,0,0.3);
-      }
-      label {
-        display: block;
-        margin-bottom: 10px;
-        font-weight: bold;
-      }
-      input[type="text"],
-      input[type="password"] {
-        display: block;
-        width: 100%;
-        padding: 10px;
-        margin-bottom: 20px;
-        border-radius: 5px;
-        border: 1px solid #ccc;
-        box-sizing: border-box;
-      }
-      input[type="submit"] {
-        display: block;
-        width: 100%;
-        padding: 10px;
-        background-color: #4CAF50;
-        color: #fff;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-      }
-      input[type="submit"]:hover {
-        background-color: #3e8e41;
-      }
-      p.error {
-        color: #ff0000;
-        font-weight: bold;
-      }
+      
     </style>
   </head>
   <body>
-    <h1>Login</h1>
-    <form action="login.php" method="post">
-      <label for="username">Username:</label>
-      <input type="text" id="username" name="username"><br>
+    <div class="wrapper">
+			<h1>Login</h1>
+			<div><span class="dot"></span></div>
+			<div><span class="dot"></span></div>
+			<div><span class="dot"></span></div>
+			<div><span class="dot"></span></div>
+			<div><span class="dot"></span></div>
+			<div><span class="dot"></span></div>
+			<div><span class="dot"></span></div>
+			<div><span class="dot"></span></div>
+			<div><span class="dot"></span></div>
+			<div><span class="dot"></span></div>
+			<div><span class="dot"></span></div>
+			<div><span class="dot"></span></div>
+			<div><span class="dot"></span></div>
+			<div><span class="dot"></span></div>
+			<div><span class="dot"></span></div>
+		</div>
+    <div class="box">
+      <form action="login.php" method="post">
+         <label for="username">Username:</label>
+         <input type="text" id="username" name="username"><br>
 
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password"><br>
-      <?php if (isset($_GET['error'])) { ?>
-        <p class="error"><?php echo $_GET['error']; ?></p>
-    <?php } ?>
-      <input type="submit" value="Login">
-    </form>
+         <label for="password">Password:</label>
+         <input type="password" id="password" name="password"><br>
+            <div class="message">
+            <?php if (isset($_GET['error'])) { ?>
+              <p class="error"><?php echo $_GET['error']; ?></p>
+            <?php } ?>
+            </div>
+        <input type="submit" value="Login">
+      </form>
+    </div>
   </body>
 </html>
